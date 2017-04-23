a21de6cb0dc343561424259aa8a235a8b273fe52
Bartosz Kolada <bartkolada@gmail.com>
2017-04-23 18:19:26 +0200

    Added -f command to append anyway even if alias exists

--- alias_manager/mali.py
+++ alias_manager/mali.py
@@ -26,31 +26,33 @@ def print_current_aliases():
 
 
 def check_if_alias_exists(alias):
-    return filter(lambda x: x[0]==alias, read_to_list())
+    return filter(lambda x: x[0] == alias, read_to_list())
 
 
 def create_alias(alias, command, force=None):
     if not force:
         existing_aliases = check_if_alias_exists(alias)
         if existing_aliases:
-            print("Alias already exists {}={}".format(*existing_aliases[0]))
-        return
-    with open(aliases_file, "a") as af:
-        af.write('alias {}="{}"\n'.format(alias, command))
+            print("Alias already exists")
+            for line in existing_aliases:
+                print("{}={}".format(*line))
+    else:
+        with open(aliases_file, "a") as af:
+            af.write('alias {}="{}"\n'.format(alias, command))
+            print('Alias added {}="{}"'.format(alias, command))
 
 
 def print_usage():
-    print("""
-    Usage mali:
-        -c - prints aliases defined in ~/.zshaliases 
-        [alias] [command] - adds new alias to ~/.zshaliases
-        -r [alias] - removes alias
-    """)
+    print("""Usage mali:
+    -c - prints aliases defined in ~/.zshaliases 
+    [alias] [command] - adds new alias to ~/.zshaliases
+    -r [alias] - removes alias
+""")
 
 
 def write_aliases_to_file(aliases):
     with open(aliases_file, "w") as af:
-        af.writelines(map(lambda x: 'alias {}={}\n'.format(x[0], x[1]), aliases))
+        af.writelines(map(lambda x: 'alias {}={}\n'.format(*x), aliases))
 
 
 def remove_alias(alias):
@@ -58,17 +60,18 @@ def remove_alias(alias):
     aliases_without_alias = filter(lambda x: alias != x[0], aliases)
     write_aliases_to_file(aliases_without_alias)
     if len(aliases) != len(aliases_without_alias):
-        print("Removed {}".format(alias))
+        print("Removed all occurences of {}".format(alias))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print_usage()
     elif "-c" in sys.argv:
         print_current_aliases()
     elif "-r" in sys.argv and len(sys.argv) == 3:
         remove_alias(sys.argv[2])
-    elif len(sys.argv) == 3:
-        create_alias(sys.argv[-2], sys.argv[-1])
+    elif len(sys.argv) >= 3:
+        create_alias(sys.argv[1], sys.argv[2], "-f" in sys.argv[3:])
     else:
         print("Not recognizable arguments {}".format(sys.argv[1:]))
         print_usage()
